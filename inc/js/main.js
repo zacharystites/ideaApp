81ea7f69e796fc5c4b31dc636f0b2af2f5201dbe
zachary stites <zacharystites@fullsail.edu>
2013-08-29 15:31:21 -0400

    Pushing final version up

--- inc/js/main.js
+++ inc/js/main.js
@@ -5,6 +5,8 @@ idealy.run(['angularFireAuth', function(angularFireAuth){
 	angularFireAuth.initialize(url, {'name':'user', 'path':'/'});
 }]);
 
+var colors = ["icon-green", "icon-pink", "icon-red", "icon-teal", "icon-orange", "icon-blue"];
+
 idealy.config(function($routeProvider){
 	$routeProvider.when("/", {
 		templateUrl: "views/signup.html",
@@ -22,7 +24,11 @@ idealy.config(function($routeProvider){
 		templateUrl: "views/viewIdea.html",
 		controller: "idea",
 		authRequired: true
-	}).when("/idea", {
+	}).when("/idea/new", {
+		templateUrl: "views/newidea.html",
+		controller: "idea",
+		authRequired: true
+	}).when("/idea/:type/:projectId", {
 		templateUrl: "views/newidea.html",
 		controller: "idea",
 		authRequired: true
@@ -37,6 +43,7 @@ idealy.controller("Core", ['$scope', '$location', function($scope, $location){
 
 idealy.controller("user",['$scope', 'angularFireAuth',"$location", function($scope, angularFireAuth, $location){
 	$scope.location = $location;
+	$scope.logintext = "log in";
 	$scope.signup = function(){
 		if($scope.validatePassword()){
 			angularFireAuth.createUser($scope.user.email, $scope.user.password, function(user){
@@ -51,7 +58,9 @@ idealy.controller("user",['$scope', 'angularFireAuth',"$location", function($sco
 		
 	};
 	$scope.login = function(){
+		$scope.logintext = "loading..";
 		angularFireAuth.login("password", $scope.user).then(function(){
+
 			$location.path("/ideas");
 		});
 	}
@@ -65,28 +74,91 @@ idealy.controller("user",['$scope', 'angularFireAuth',"$location", function($sco
 	}
 }]);
 
-idealy.controller("ideas", ['$scope', 'angularFireCollection', function($scope, angularFireCollection){
+idealy.controller("ideas", ['$scope', 'angularFireCollection', '$location', function($scope, angularFireCollection, $location){
 	var url="https://idea-app.firebaseio.com/idea";
 	$scope.bodyClass='';
 	$scope.ideas = angularFireCollection(url);
+	$scope.icons = {
+		finance: "icon-briefcase",
+		eCommerce: "icon-shopping-cart",
+		food: "icon-glass",
+		health: "icon-heart",
+		webApp: "icon-globe",
+		phoneApp: "icon-camera"
+	}
+	$scope.deleteStatus = {state:false};
+	console.log('$scope.deleteStatus',$scope.deleteStatus);
+
+	$scope.viewIdea = function(id) {
+		$location.path("/idea/"+id);
+	}
+
+	$scope.deleteIdea = function(id,$event) {
+		$scope.ideas.remove(id);
+		$event.stopPropagation();
+		$event.cancelBubble = true;
+	}	
+
+	$scope.deleteStatus = function() {
+		if ($scope.deleteStatus.state == true){
+			$scope.deleteStatus.state = false;
+		}else{
+			$scope.deleteStatus.state = true;
+		}
+	}
 }]);
 
-idealy.controller("idea", ['$scope', 'angularFireCollection', '$rootScope', '$routeParams', function($scope, angularFireCollection, $rootScope, $routeParams){
+idealy.controller("idea", ['$scope', 'angularFireCollection', '$rootScope', '$routeParams', '$location', '$timeout', function($scope, angularFireCollection, $rootScope, $routeParams, $location,$timeout){
 	var url="https://idea-app.firebaseio.com/idea";
+	$scope.location = $location;
 	$scope.bodyClass='';
-	var collection = angularFireCollection(url,function(data)
+
+
+
+	$scope.ideas = angularFireCollection(url);
+
+	var time_to_end = 4000;
+	var timer = function(){
+		$timeout(function()
 		{
-			if($routeParams.projectId)
+			time_to_end--;
+			if($scope.ideas.length === 0 && time_to_end > 0)
 			{
-				console.log(data.val());
-				$scope.idea = data.val()[ $routeParams.projectId ] ;	
+				timer();
 			}
-			
-		});
+			else
+			{
+				for(key in $scope.ideas)
+				{
+					if($scope.ideas[key].$id == $routeParams.projectId)
+					{
+						$scope.idea = $scope.ideas[key];
+
+						break;
+					}
+				}
+			}
+		},10)
+	}
+		
+	timer();
+
 	$scope.saveIdea = function(){
 		$scope.bodyClass='';
 		$scope.idea.userId = $rootScope.user.id;
 		$scope.idea.createdDate = new Date().getTime();
-		collection.add($scope.idea);
+		if($routeParams.type == "edit")
+		{
+			$scope.ideas.update($scope.idea, function(){
+				window.location = '#/ideas';
+			});
+		}
+		else
+		{
+			$scope.ideas.add($scope.idea, function(){
+				window.location = '#/ideas';
+			});
+		}
+		
 	};
 }]);
